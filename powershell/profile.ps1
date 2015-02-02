daef043b3f2b175f97731e0577718c85acd77990
David Rusk <david.rusk@onebitlabs.com>
2015-02-02 11:27:44 -0800

    Changed default Powershell colours to black background with white text.

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -7,4 +7,10 @@ if ($host.Name -eq 'ConsoleHost')
 	Set-PSReadlineOption -EditMode Emacs
 	Set-PSReadlineKeyHandler -Key Ctrl+p -Function PreviousHistory
 	Set-PSReadlineKeyHandler -Key Ctrl+n -Function NextHistory
+
+	# Change default colours
+	$ui = (Get-Host).UI.RawUI
+	$ui.ForegroundColor = "white"
+	$ui.BackgroundColor = "black"
+	Clear-Host
 }
